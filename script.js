d0bb0494efe088300bc6ae3b10b492ce5464bf8a
akshithreddypalvai2005-hash <akshithreddypalvai2005@gmail.com>
2026-06-13 09:46:05 +0530

    scrript.js

--- script.js
+++ script.js
@@ -1,16 +1,44 @@
 function generateResume(){
 
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let education = document.getElementById("education").value;
-    let skills = document.getElementById("skills").value;
-    let experience = document.getElementById("experience").value;
+document.getElementById("rName").innerText =
+document.getElementById("name").value;
 
-    document.getElementById("resume").innerHTML = `
-        <h2>${name}</h2>
-        <p><b>Email:</b> ${email}</p>
-        <p><b>Education:</b> ${education}</p>
-        <p><b>Skills:</b> ${skills}</p>
-        <p><b>Experience:</b> ${experience}</p>
-    `;
+document.getElementById("rJob").innerText =
+document.getElementById("job").value;
+
+document.getElementById("rEmail").innerText =
+document.getElementById("email").value;
+
+document.getElementById("rPhone").innerText =
+document.getElementById("phone").value;
+
+document.getElementById("rProfile").innerText =
+document.getElementById("profile").value;
+
+document.getElementById("rSkills").innerText =
+document.getElementById("skills").value;
+
+document.getElementById("rEducation").innerText =
+document.getElementById("education").value;
+
+document.getElementById("rExperience").innerText =
+document.getElementById("experience").value;
+
+const file = document.getElementById("photo").files[0];
+
+if(file){
+
+const reader = new FileReader();
+
+reader.onload = function(e){
+document.getElementById("profilePic").src =
+e.target.result;
+}
+
+reader.readAsDataURL(file);
+}
+}
+
+function downloadResume(){
+window.print();
 }
